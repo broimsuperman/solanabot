b4f7ebe6f5526fac7e0a31b98d4f04a20c20840b
broimsuperman <77177009+broimsuperman@users.noreply.github.com>
2025-01-17 14:12:41 -0500

    Create bundle.ts

---
+++ bundle/bundle.ts
@@ -0,0 +1,19 @@
+// Work in progress!
+
+//Steps
+// Use Master Wallet (defined in .env)
+// Get the amount of wallets we want to create
+// get the amount of solana we want to distribute in total
+// create new wallets 
+// transfer sol to each wallet
+// determine if using bundling after master wallet buys
+// or if all at the same time
+// get the stop loss and take profit
+// when close, bot will sell twice and create 2 or 4 more buys
+// if safe bundling is enabled
+// once a break even has been established,
+// the bot will sell all at once
+// bots will transfer to master wallet or burn wallet
+// bot workers will destruct, but save private keys into a file
+// if something goes wrong, add an option to drain workers wallets into master wallet
+
